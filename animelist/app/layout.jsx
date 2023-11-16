9c639129ef8cb4577f009612fcc9f7ffc2fc02d5
Ikhram Syamsuri <117132471+ikhramsyamsuri@users.noreply.github.com>
2023-11-16 16:17:47 +0700

    change layout.jsx

--- animelist/app/layout.jsx
+++ animelist/app/layout.jsx
@@ -1,8 +1,8 @@
 import Navbar from './components/Navbar/index'
-import { Gabarito } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 
-const gabarito = Gabarito({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Anime Iyom',
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${gabarito.className} bg-owned-accent`}>
+      <body className={`${inter.className} bg-owned-accent`}>
         <Navbar/>
         {children}
       </body>
